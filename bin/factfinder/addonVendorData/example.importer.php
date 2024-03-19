179ab5fc059f9a98e30d4e36b794d058f1414df2
Daniel Kussin <daniel.kussin@kussin.de>
2024-03-19 13:55:46 +0100

    This commit refs #60886: Added basic importer files

---
+++ bin/factfinder/addonVendorData/example.importer.php
@@ -0,0 +1,17 @@
+<?php
+// IMPORT FILE NAME
+
+
+// MAPPING
+
+
+
+
+// ----------------- ### NO CHANGES BEHIND THIS LINE ### -----------------
+
+// INIT
+error_reporting (E_ALL);
+ini_set ('display_errors', 'On');
+
+// INIT OXID
+require_once '../../bootstrap.php';
