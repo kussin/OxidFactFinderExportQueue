cb0a2c4e9212a5d08b485764e707e87f1c6a2857
Daniel Kussin <daniel.kussin@kussin.de>
2024-11-19 14:46:39 +0100

    This commit refs #65610: Added logic

--- modules/wmdk/wmdkffexportqueue/Traits/Ajax/ResetTrait.php
+++ modules/wmdk/wmdkffexportqueue/Traits/Ajax/ResetTrait.php
@@ -2,6 +2,8 @@
 
 namespace Wmdk\FactFinderQueue\Traits\Ajax;
 
+use OxidEsales\Eshop\Core\DatabaseProvider;
+
 trait ResetTrait
 {
     /**
@@ -18,6 +20,72 @@ trait ResetTrait
             $this->_aResponse['validation_errors'][] = 'ERROR_NO_OXID_GIVEN';
 
             return FALSE;
+        }
+
+        $sArticles = 'SELECT DISTINCT 
+            OXID,
+            ProductNumber,
+            MasterProductNumber
+        FROM
+            wmdk_ff_export_queue
+        WHERE 
+            ProductNumber IN (
+                SELECT DISTINCT
+                    ProductNumber
+                FROM 
+                    wmdk_ff_export_queue
+                WHERE 
+                    (OXID = "' . $sOxid .'")
+            )
+            OR 
+            MasterProductNumber IN (
+                SELECT DISTINCT
+                    ProductNumber
+                FROM 
+                    wmdk_ff_export_queue
+                WHERE 
+                    (OXID = "' . $sOxid .'")
+            )
+            OR MasterProductNumber = (
+                SELECT DISTINCT 
+                    MasterProductNumber
+                FROM 
+                    wmdk_ff_export_queue
+                WHERE 
+                    (OXID = "' . $sOxid .'")
+            );';
+
+        $oResult = DatabaseProvider::getDb(FALSE)->select($sArticles);
+
+        if ($oResult != FALSE && $oResult->count() > 0) {
+            $this->_aResponse['reseted'] = array();
+
+            while (!$oResult->EOF) {
+                $aData = $oResult->getFields();
+
+                $sQuery = 'UPDATE
+                    wmdk_ff_export_queue
+                SET
+                    LASTSYNC = "0000-00-00 00:00:00",
+                    ProcessIp = "' . $this->_getProcessIp() . '",
+                    OXTIMESTAMP = "0000-00-00 00:00:00"
+                WHERE
+                    (OXID = "' . $aData['OXID'] . '")';
+
+                try {
+                    DatabaseProvider::getDb()->execute($sQuery);
+
+                    // LOG
+                    $this->_aResponse['reseted'][] = $aData['OXID'];
+
+                } catch (Exception $oException) {
+                    // ERROR
+                    $this->_aResponse['success'] = FALSE;
+                    $this->_aResponse['system_errors'][] = 'ERROR_COULD_NOT_RESET: ' . $aData['OXID'];
+                }
+
+                $oResult->fetchRow();
+            }
         }
 
         return FALSE;
