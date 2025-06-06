70423cc0b13d85ad4fa29c79762feade6ea794c5
Daniel Kussin <daniel.kussin@kussin.de>
2025-06-06 15:52:29 +0200

    This commit refs #66948: Added simple building logic

--- modules/wmdk/wmdkffexportqueue/Traits/ProductNameBuilderTrait.php
+++ modules/wmdk/wmdkffexportqueue/Traits/ProductNameBuilderTrait.php
@@ -26,12 +26,66 @@ trait ProductNameBuilderTrait
         }
 
         // PREPARE PRODUCT NAME
-        // TODO: Add logic to replace the placeholders in the pattern with actual data
-//        $aData[$this->_aProductNameBuilderColumns[$sFieldName]] = implode(' ', [
-//            $aData[$this->_aProductNameBuilderColumns['Marke']],
-//            $aData[$this->_aProductNameBuilderColumns[$sFieldName]],
-//        ]);
+        // EXTACT PLACEHOLDERS
+        preg_match_all('/\[([^\]]+)\]/', $sPattern, $aMatches);
+
+        foreach ($aMatches[1] as $sPlaceholder) {
+            if (preg_match('/^([a-zA-Z_][a-zA-Z0-9_]*)\(([^)]+)\)$/', $sPlaceholder, $parts)) {
+                // COMPLEX PLACEHOLDER
+                $sValue = $this->_getPNBComplexData($aData, $parts[1], $parts[2]);
+
+            } elseif (
+                (($sPlaceholder === 'Variante') || ($sPlaceholder === 'Variant'))
+            ) {
+                // VARINATS INFORMATION
+                $sValue = $this->_getPNBVariantsData($aData);
+
+            } else {
+                // SIMPLE PLACEHOLDER
+                $sValue = $this->_getPNBSimpleData($aData, $sPlaceholder);
+            }
+
+            $sPattern = str_replace("[$sPlaceholder]", $sValue, $sPattern);
+        }
+
+        // SET PRODUCT NAME
+        if (
+            isset($this->_aProductNameBuilderColumns[$sFieldName])
+            && (strlen($sPattern) > 0)
+        ) {
+            $aData[$this->_aProductNameBuilderColumns[$sFieldName]] = $sPattern;
+        }
 
         return $aData;
+    }
+
+    private function _getPNBSimpleData($aData, $sAttributeName)
+    {
+        return isset($aData[$this->_aProductNameBuilderColumns[$sAttributeName]])
+            ? $aData[$this->_aProductNameBuilderColumns[$sAttributeName]] : '';
+    }
+
+    private function _getPNBComplexData($aData, $sDataKey, $sAttributeName)
+    {
+        // TODO: Implement logic to retrieve complex data based on the key and attribute name
+        return '';
+    }
+
+    private function _getPNBVariantsData($aData)
+    {
+        $sProductNumber = $aData[$this->_aProductNameBuilderColumns['ProductNumber']];
+        $sMasterProductNumber = $aData[$this->_aProductNameBuilderColumns['MasterProductNumber']];
+
+        if ($sProductNumber === $sMasterProductNumber) {
+            return '';
+        }
+
+        // TODO: Implement logic to retrieve variant data
+//        return implode(' ', [
+//            '<label>',
+//            $sMasterProductNumber,
+//            ':</label>',
+//            $sProductNumber,
+//        ]);
     }
 }
